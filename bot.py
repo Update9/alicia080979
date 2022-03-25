93c7a057f5fcb351361693486038ca5e6f039b45
Update9 <97268451+Update9@users.noreply.github.com>
2022-03-26 01:42:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def main():
         listen="0.0.0.0",
         port=int(PORT),
         url_path=TOKEN,
-        webhook_url='https://aiop57.herokuapp.com/' + 5103049162:AAE49wuhia1kV7X81wjujdVLbrMOGWw6BxM 
+        webhook_url='https://alsoeee.herokuapp.com/' + 5103049162:AAE49wuhia1kV7X81wjujdVLbrMOGWw6BxM 
     )
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
